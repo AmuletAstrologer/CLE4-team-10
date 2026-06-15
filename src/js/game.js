6ba0951c0e163eb2bb8dacc197f9a3531779ad17
Marit57 <m.reijken@ziggo.nl>
2026-06-15 11:08:59 +0200

    volledige import van game.js gedaan

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,7 @@
-import { Engine } from 'excalibur';
 import '../css/style.css'
+import { Actor, Engine, Vector, DisplayMode, SolverStrategy } from "excalibur"
+import { Resources, ResourceLoader } from './resources.js'
+import { Start } from './scenes/start.js'
 
 export class Game extends Engine {
     
