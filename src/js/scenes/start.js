84d7490e04b92fc0ac647bd338f8dabe92161652
Marit57 <m.reijken@ziggo.nl>
2026-06-15 13:52:01 +0200

    werkend font

--- src/js/scenes/start.js
+++ src/js/scenes/start.js
@@ -1,26 +1,16 @@
 import { Color, FontUnit, Label, Scene } from 'excalibur';
 import { Resources } from '../resources.js'
 import { Background } from "../background/background.js"
+import { StartLabel } from '../startlabel.js';
 
 
 export class Start extends Scene {
+
     onInitialize(engine) {
         const background = new Background();
         this.add(background);
 
-        this.nameLabel = new Label({
-            text: "SUSTAINSPHERE",
-            font: Resources.PixelFont.toFont({
-                unit: FontUnit.Px,
-                size: 28,
-                color: Color.White
-            }),
-            x: 10,
-            y: 10,
-        })
-
-        this.add(this.nameLabel);
+        this.add(new StartLabel);
     }
-
 
 }
