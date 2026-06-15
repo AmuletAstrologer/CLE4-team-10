ed8f20c42c284da6ea87aef5eb87cac6226cd782
Marit57 <m.reijken@ziggo.nl>
2026-06-15 11:53:45 +0200

    ruimte afval images
    alle individuele images van het ruimteafval

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,6 +1,21 @@
 import { ImageSource, Sound, Resource, Loader, FontSource } from 'excalibur'
 
-const Resources = {}
+const Resources = {
+    Background: new ImageSource('images/background.png'),
+    AfvalAirtank: new ImageSource('images/afvalairtank.png'),
+    AfvalCilinder: new ImageSource('images/afvalcilinder.png'),
+    AfvalFragment: new ImageSource('images/afvalfragment.png'),
+    AfvalHaak: new ImageSource('images/afvalhaak.png'),
+    AfvalHelm: new ImageSource('images/afvalhelm.png'),
+    AfvalModule: new ImageSource('images/afvalmodule.png'),
+    AfvalPaneel: new ImageSource('images/afvalpaneel.png'),
+    AfvalPlaat: new ImageSource('images/afvalplaat.png'),
+    AfvalSatelliet: new ImageSource('images/afvalsatelliet.png'),
+    AfvalSchroef1: new ImageSource('images/afvalschroef1.png'),
+    AfvalSchroef2: new ImageSource('images/afvalschroef2.png'),
+    AfvalSchroef3: new ImageSource('images/afvalschroef3.png'),
+    AfvalSchroef4: new ImageSource('images/afvalschroef4.png'),
+}
 
 const ResourceLoader = new Loader()
 for (let res of Object.values(Resources)) {
