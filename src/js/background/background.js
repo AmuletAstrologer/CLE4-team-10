2854487ceaf841ce9ccf62a25a17840974297bee
Sel-Michiel <selmichiel68@gmail.com>
2026-06-15 12:04:32 +0200

    Bg Issue

--- src/js/background/background.js
+++ src/js/background/background.js
@@ -1,22 +1,11 @@
-import { Engine, Actor} from "excalibur"
+import { Engine, Actor, Vector} from "excalibur"
 import { Resources, ResourceLoader } from "../resources.js"
 
 
 export class Background extends Actor {
 
-    constructor() {
-        super({
-            width: 1280,
-            height: 720,
-            x: 1280/2,
-            y: 720/2, 
-
-        })
-        
-    }
-
     onInitialize (engine){
         this.graphics.use (Resources.Background.toSprite());
-        this.z = -1; 
+        this.anchor = new Vector(0,0);
     }
 }
