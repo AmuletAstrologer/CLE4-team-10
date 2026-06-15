7977a2ecbc0c07795ebfdfe83a30bd40430c8090
Marit57 <m.reijken@ziggo.nl>
2026-06-15 12:33:23 +0200

    fonts map en start

--- src/js/scenes/start.js
+++ src/js/scenes/start.js
@@ -1,22 +1,17 @@
-import { Actor, Engine, Vector, DisplayMode, SolverStrategy } from "excalibur"
-import { Resources, ResourceLoader } from '../resources.js'
+import { Label, Scene } from 'excalibur';
+import { Resources } from '../resources.js'
 import { Background } from "../background/background.js"
 
 
-export class Start extends Engine {
-    constructor() {
-        super({
-            width: 1280,
-            height: 720,
-            maxFps: 60,
-            displayMode: DisplayMode.FitScreen,
-        })
-        this.start(ResourceLoader).then(() => this.startGame())
-    }
-
+export class Start extends Scene {
+    
     onInitialize(engine) {
         const background = new Background();
         this.add(background);
+
+        const nameLabel = new Label({
+            text: "SUSTAINSPHERE",
+        })
     }
 
 
