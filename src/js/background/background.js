ae53e16c29d6a32518932cdf02bf9e9fb18b029d
Luuk709 <1104642@hr.nl>
2026-06-16 09:39:34 +0200

    merged #17

---
+++ src/js/background/background.js
@@ -0,0 +1,11 @@
+import { Engine, Actor, Vector} from "excalibur"
+import { Resources, ResourceLoader } from "../resources.js"
+
+
+export class Background extends Actor {
+
+    onInitialize (engine){
+        this.graphics.use (Resources.Background.toSprite());
+        this.anchor = new Vector(0,0);
+    }
+}
