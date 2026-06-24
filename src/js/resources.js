cb468e8d0621bbdd69fe4b7a684ef38eee7dd424
Luuk709 <1104642@hr.nl>
2026-06-24 11:43:02 +0200

    new crosshair / levelone ending fixes

--- src/js/resources.js
+++ src/js/resources.js
@@ -4,7 +4,7 @@ import { Cursor } from "./objects/cursor";
 
 const Resources = {
   Background: new ImageSource("images/background.png"),
-  Cursor: new ImageSource("images/cursor.png"),
+  Cursor: new ImageSource("images/crosshair.png"),
   AfvalAirtank: new ImageSource("images/afvalairtank.png"),
   AfvalCilinder: new ImageSource("images/afvalcilinder.png"),
   AfvalFragment: new ImageSource("images/afvalfragment.png"),
