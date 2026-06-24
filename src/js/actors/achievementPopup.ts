ea9fc02a3248b7d243229df7e34e658ba728e539
Luuk709 <1104642@hr.nl>
2026-06-24 14:34:51 +0200

    Added achievement popups
    Achievement popups are now fully functional.

--- src/js/actors/achievementPopup.ts
+++ src/js/actors/achievementPopup.ts
@@ -7,29 +7,39 @@ import {
   TextAlign,
   BaseAlign,
   Engine,
+  vec,
 } from "excalibur";
 import { Resources, ResourceLoader } from "../resources";
 export class AchievementPopup extends Actor {
+  #popupLabel = new Label({
+    pos: Vector.Zero,
+    font: Resources.PixelFont.toFont({
+      size: 20,
+      unit: FontUnit.Px,
+      textAlign: TextAlign.Center,
+      baseAlign: BaseAlign.Middle,
+      color: Color.White,
+    }),
+  });
+
   private timer = 0;
+  private duration = 2000;
   constructor(text: string) {
-    super({ pos: new Vector(800, 100), z: 1000 });
-    const popupLabel = new Label({
-      text,
-      pos: Vector.Zero,
-      font: Resources.PixelFont.toFont({
-        size: 35,
-        unit: FontUnit.Px,
-        textAlign: TextAlign.Center,
-        baseAlign: BaseAlign.Middle,
-        color: Color.White,
-      }),
-    });
-    this.addChild(popupLabel);
+    super({ pos: new Vector(1200, 680), z: 1000 });
+    this.#popupLabel.text = `Achievement Unlocked: ${text}`;
+
+    this.addChild(this.#popupLabel);
   }
+  onInitialize(engine: Engine): void {
+    this.pos = vec(engine.drawWidth - 250, engine.drawHeight - 30);
+  }
+
   onPreUpdate(engine: Engine, elapsed: number): void {
     this.timer += elapsed;
     this.pos.y -= 0.05 * elapsed;
-    if (this.timer > 2000) {
+    const t = Math.min(this.timer / this.duration, 1);
+    this.graphics.opacity = 1 - t;
+    if (t >= 1 && this.#popupLabel !== undefined) {
       this.kill();
     }
   }
--- src/js/lib/achievementmanager.ts
+++ src/js/lib/achievementmanager.ts
@@ -55,32 +55,37 @@ export class AchievementManager {
     return JSON.parse(achievements);
   }
 
-  public static checkAchievements(): void {
+  public static checkAchievements(): number[] {
+    const unlocked: number[] = [];
     const achievements = AchievementManager.getAchievements();
-    const scores = getScores();
-    this.checkAchievement1(scores, achievements);
-    this.checkAchievement2(achievements);
-    // this.checkAchievement3(score)
-    this.checkAchievement4(achievements);
+    // const scores = getScores();
+    if (this.checkAchievement1(achievements)) unlocked.push(1);
+    if (this.checkAchievement2(achievements)) unlocked.push(2);
+    // if (this.checkAchievement3(score)) unlocked.push(3);
+    if (this.checkAchievement4(achievements)) unlocked.push(4);
     localStorage.setItem("achievements", JSON.stringify(achievements));
+    return unlocked;
   }
 
   static checkAchievement1(
-    scores: LevelScores,
+    // scores: LevelScores,
     achievements: Partial<Achievements>,
-  ) {
-    if (scores.levelOne === 10) {
-      const perfectHooking = achievements.achievements?.find(
-        (achievement) => achievement.name === "Perfect Hooking",
-      );
+  ): boolean {
+    // if (scores.levelOne === 10) {
+    //   const perfectHooking = achievements.achievements?.find(
+    //     (achievement) => achievement.name === "Perfect Hooking",
+    //   );
 
-      if (perfectHooking) {
-        perfectHooking.unlocked = true;
-        // currentScene.add(new AchievementPopup("Perfect Hooking"));
-      }
-    }
+    //   if (perfectHooking) {
+    //     if (perfectHooking.unlocked !== true) {
+    //       perfectHooking.unlocked = true;
+    //       return true;
+    //     }
+    //   }
+    // }
+    return false;
   }
-  static checkAchievement2(achievements: Partial<Achievements>) {
+  static checkAchievement2(achievements: Partial<Achievements>): boolean {
     const scrap = ScrapManager.getScrap();
     if (scrap > 24) {
       const scrapCollector = achievements.achievements?.find(
@@ -88,22 +93,33 @@ export class AchievementManager {
       );
 
       if (scrapCollector) {
-        scrapCollector.unlocked = true;
+        if (!scrapCollector.unlocked) {
+          scrapCollector.unlocked = true;
+          return true;
+        }
       }
     }
+    return false;
   }
-  static checkAchievement3(achievements: Partial<Achievements>, score: number) {
+  static checkAchievement3(
+    achievements: Partial<Achievements>,
+    score: number,
+  ): boolean {
     if (score > 14) {
       const highscore = achievements.achievements?.find(
         (achievement) => achievement.name === "High Score",
       );
 
       if (highscore) {
-        highscore.unlocked = true;
+        if (!highscore.unlocked) {
+          highscore.unlocked = true;
+          return true;
+        }
       }
     }
+    return false;
   }
-  static checkAchievement4(achievements: Partial<Achievements>) {
+  static checkAchievement4(achievements: Partial<Achievements>): boolean {
     let values = 0;
     for (let index = 0; index < upgradeTypes.length; index++) {
       const element = upgradeTypes[index];
@@ -116,9 +132,13 @@ export class AchievementManager {
       );
 
       if (recycleMaster) {
-        recycleMaster.unlocked = true;
+        if (!recycleMaster.unlocked) {
+          recycleMaster.unlocked = true;
+          return true;
+        }
       }
     }
+    return false;
   }
   public static isUnlocked(name: AchievementNames): boolean {
     return (
